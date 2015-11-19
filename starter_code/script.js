6424c5bddd199341d32b625a4c6da2f941185428
lguzman2017 <lguzman@cristoreyny.org>
2015-11-19 13:16:36 +0000

    Rock Paper Scissors

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,14 +1,36 @@
 var userPoint = 0;
 var aiPoint = 0;
 
-// This function returns the selection of the computer
 function getAISelection() {
-    //TODO: randomly choose between 'rock', 'paper', or 'scissors'
-}
+    var Options= ["rock","paper","scissors"];
+  var aiOption= Options[Math.floor(Math.random()* Options.length)];
+  alert(aiOption);
+  return aiOption;
+} 
 
 // This function picks the winner
 function pickWinner(userValue, aiValue) {
-    //TODO: pick the correct winner: user or ai
+   if (userValue == aiValue) {
+     return "draw" ;
+   }
+  if (userValue== "rock"&& aiValue== "paper") {
+    return "ai";
+  }
+   if (userValue== "rock"&& aiValue== "scissors") {
+    return "user";
+   }
+   if (userValue== "paper"&& aiValue== "rock") {
+    return "user";
+   }
+   if (userValue== "paper"&& aiValue== "scissors") {
+    return "ai";
+   }
+   if (userValue== "scissors"&& aiValue== "paper") {
+    return "user";
+   }
+  if (userValue== "scissors"&& aiValue== "rock") {
+    return "ai";
+  }
     //TODO: Add one point for the winner
 }
 
@@ -25,15 +47,27 @@ function evaluate(evt) {
     var winner = pickWinner(userValue, aiValue);
 
     if ( 'user' === winner ) {
-        $('#message').delay(50).text('You have won!, Click a box to play again');
+        $('#message').delay(50).text('You have won!, Click a box to play again'); userPoint++;
     } else if ( winner === 'draw' ) {
         $('#message').delay(50).text('Draw! Click a box to play again');
     } else {
-        $('#message').delay(50).text('You have lost!, Click a box to play again');
+        $('#message').delay(50).text('You have lost!, Click a box to play again'); aiPoint++;
     }
 }
 
 // This function runs on page load
 $(document).ready(function(){
-
+$("#rock").click(function(evt) {
+  evaluate(evt);
+  });
+  $("#paper").click(function(evt) {
+    evaluate(evt);
+  });
+  $("#scissors").click(function(evt) {
+    evaluate(evt);
+  });
+  
 });
+
+  
+  
